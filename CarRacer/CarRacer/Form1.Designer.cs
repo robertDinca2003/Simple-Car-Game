f6456daa88c07bdef9097cc96006d52b16d1c90f
Robert Dinca <71851178+robertDinca2003@users.noreply.github.com>
2022-10-08 12:24:06 +0300

    Add files via upload

---
+++ CarRacer/CarRacer/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace CarRacer
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerCloack = new System.Windows.Forms.Timer(this.components);
+            this.rightBorder = new System.Windows.Forms.PictureBox();
+            this.leftBorder = new System.Windows.Forms.PictureBox();
+            this.GameName = new System.Windows.Forms.Label();
+            this.btnStartGame = new System.Windows.Forms.Button();
+            this.BestScoreLabel = new System.Windows.Forms.Label();
+            this.panelGame = new System.Windows.Forms.Panel();
+            this.btn_back = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.rightBorder)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftBorder)).BeginInit();
+            this.panelGame.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timerCloack
+            // 
+            this.timerCloack.Tick += new System.EventHandler(this.timerCloack_Tick);
+            // 
+            // rightBorder
+            // 
+            this.rightBorder.BackColor = System.Drawing.Color.White;
+            this.rightBorder.Location = new System.Drawing.Point(1000, 1);
+            this.rightBorder.Name = "rightBorder";
+            this.rightBorder.Size = new System.Drawing.Size(15, 1877);
+            this.rightBorder.TabIndex = 3;
+            this.rightBorder.TabStop = false;
+            this.rightBorder.Visible = false;
+            // 
+            // leftBorder
+            // 
+            this.leftBorder.BackColor = System.Drawing.Color.White;
+            this.leftBorder.Location = new System.Drawing.Point(34, 1);
+            this.leftBorder.Name = "leftBorder";
+            this.leftBorder.Size = new System.Drawing.Size(15, 1877);
+            this.leftBorder.TabIndex = 4;
+            this.leftBorder.TabStop = false;
+            this.leftBorder.Visible = false;
+            // 
+            // GameName
+            // 
+            this.GameName.AutoSize = true;
+            this.GameName.BackColor = System.Drawing.Color.Red;
+            this.GameName.Font = new System.Drawing.Font("Montserrat Black", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GameName.ForeColor = System.Drawing.Color.Gold;
+            this.GameName.Location = new System.Drawing.Point(60, 148);
+            this.GameName.Name = "GameName";
+            this.GameName.Size = new System.Drawing.Size(839, 199);
+            this.GameName.TabIndex = 0;
+            this.GameName.Text = "Car Racer";
+            // 
+            // btnStartGame
+            // 
+            this.btnStartGame.BackColor = System.Drawing.Color.Red;
+            this.btnStartGame.Font = new System.Drawing.Font("Montserrat Black", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartGame.ForeColor = System.Drawing.Color.Gold;
+            this.btnStartGame.Location = new System.Drawing.Point(261, 627);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(421, 103);
+            this.btnStartGame.TabIndex = 1;
+            this.btnStartGame.Text = "Start Game";
+            this.btnStartGame.UseVisualStyleBackColor = false;
+            this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
+            // 
+            // BestScoreLabel
+            // 
+            this.BestScoreLabel.AutoSize = true;
+            this.BestScoreLabel.Font = new System.Drawing.Font("Montserrat Black", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BestScoreLabel.ForeColor = System.Drawing.Color.Gold;
+            this.BestScoreLabel.Location = new System.Drawing.Point(94, 912);
+            this.BestScoreLabel.Name = "BestScoreLabel";
+            this.BestScoreLabel.Size = new System.Drawing.Size(296, 61);
+            this.BestScoreLabel.TabIndex = 2;
+            this.BestScoreLabel.Text = "Best Score:";
+            // 
+            // panelGame
+            // 
+            this.panelGame.Controls.Add(this.label3);
+            this.panelGame.Controls.Add(this.btn_back);
+            this.panelGame.Controls.Add(this.label2);
+            this.panelGame.Controls.Add(this.BestScoreLabel);
+            this.panelGame.Controls.Add(this.btnStartGame);
+            this.panelGame.Controls.Add(this.GameName);
+            this.panelGame.Location = new System.Drawing.Point(48, 1);
+            this.panelGame.Name = "panelGame";
+            this.panelGame.Size = new System.Drawing.Size(946, 1500);
+            this.panelGame.TabIndex = 5;
+            // 
+            // btn_back
+            // 
+            this.btn_back.BackColor = System.Drawing.Color.Red;
+            this.btn_back.Enabled = false;
+            this.btn_back.Font = new System.Drawing.Font("Montserrat Black", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_back.ForeColor = System.Drawing.Color.Gold;
+            this.btn_back.Location = new System.Drawing.Point(261, 736);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(421, 103);
+            this.btn_back.TabIndex = 4;
+            this.btn_back.Text = "Back To Meniu";
+            this.btn_back.UseVisualStyleBackColor = false;
+            this.btn_back.Visible = false;
+            this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat Black", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Gold;
+            this.label2.Location = new System.Drawing.Point(32, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(283, 44);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Current Score: ";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Montserrat Black", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Gold;
+            this.label3.Location = new System.Drawing.Point(157, 463);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(584, 99);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Current Score";
+            this.label3.Visible = false;
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(1058, 1444);
+            this.Controls.Add(this.panelGame);
+            this.Controls.Add(this.leftBorder);
+            this.Controls.Add(this.rightBorder);
+            this.MaximumSize = new System.Drawing.Size(1080, 1500);
+            this.MinimumSize = new System.Drawing.Size(1080, 1500);
+            this.Name = "Game";
+            this.Text = "CarRacer";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Game_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Game_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.rightBorder)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftBorder)).EndInit();
+            this.panelGame.ResumeLayout(false);
+            this.panelGame.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timerCloack;
+        private System.Windows.Forms.PictureBox rightBorder;
+        private System.Windows.Forms.PictureBox leftBorder;
+        private System.Windows.Forms.Label GameName;
+        private System.Windows.Forms.Button btnStartGame;
+        private System.Windows.Forms.Label BestScoreLabel;
+        private System.Windows.Forms.Panel panelGame;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_back;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CarRacer/CarRacer/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CarRacer
+{
+    public partial class Game : Form
+    {
+        public Game()
+        {
+
+            InitializeComponent();
+        }
+
+        int currentScore;
+
+        int bestScore = 0;
+
+        int lifes;
+
+        int speed;
+
+        int gameStarted = 0;
+
+        int timeCounter = 0;
+
+        PictureBox[] enemy = new PictureBox[10];
+        PictureBox[] coins = new PictureBox[4];
+        PictureBox[] hearts = new PictureBox[3];
+        PictureBox[] lines = new PictureBox[5];
+
+        PictureBox player = new PictureBox();
+
+        Random rand = new Random();
+
+        private void btnStartGame_Click(object sender, EventArgs e)
+        {
+            GameName.Visible = false;
+            BestScoreLabel.Visible = false;
+            Game.ActiveForm.BackColor = Color.Gray;
+            btnStartGame.Enabled = false;
+            btnStartGame.Visible = false;
+            leftBorder.Visible = true;
+            rightBorder.Visible = true;
+            label2.Visible = true;
+            timerCloack.Enabled = true;
+
+            currentScore = 0;
+            lifes = 3;
+            speed = 10;
+            gameStarted = 1;
+
+            label2.Text = "Current Score: " + currentScore.ToString();
+            label2.BackColor = Color.Transparent;
+
+            PictureBox temp1 = new PictureBox();
+            panelGame.Controls.Add(temp1);
+            temp1.Height = 200;
+            temp1.Width = 100;
+            temp1.Location = new Point(270, 650);
+            temp1.Visible = true;
+            string path = Directory.GetCurrentDirectory();
+            path = path + "\\Resources\\copycar.png";
+            Image playerCar = Image.FromFile(path);
+            temp1.Image = playerCar;
+            temp1.SizeMode = PictureBoxSizeMode.StretchImage;
+            temp1.BackColor = Color.Transparent;
+            player = temp1;
+            player.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+
+            for (int i = 0; i < 5; i++)
+            {
+                PictureBox temp2 = new PictureBox();
+                panelGame.Controls.Add(temp2);
+                enemy[i] = temp2;
+                enemy[i].Height = 200;
+                enemy[i].Width = 100;
+                enemy[i].Visible = true;
+                enemy[i].Location = new Point(rand.Next(1, 5) * 100, -i * 400);
+                string path2 = Directory.GetCurrentDirectory();
+                path2 = path2 + "\\Resources\\obstacleCar.png";
+                enemy[i].Image = Image.FromFile(path2);
+                enemy[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                enemy[i].Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                PictureBox temp3 = new PictureBox();
+                panelGame.Controls.Add(temp3);
+                temp3.Height = 75;
+                temp3.Width = 75;
+                temp3.Visible = true;
+                temp3.Location = new Point(rand.Next(1, 10) * 50, -i * 300);
+                string path3 = Directory.GetCurrentDirectory();
+                path3 = path3 + "\\Resources\\coin.png";
+                temp3.Image = Image.FromFile(path3);
+                temp3.SizeMode = PictureBoxSizeMode.StretchImage;
+                coins[i] = temp3; 
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                PictureBox temp = new PictureBox();
+                panelGame.Controls.Add(temp);
+                temp.Height=100;
+                temp.Width=10;
+                temp.Location =new Point(300,  i*220);
+                temp.BackColor = Color.White;
+                temp.Visible = true;
+                
+                lines[i] = temp;
+                
+                
+
+            }
+            
+            
+
+
+        }
+
+        void moveLines()
+        {
+            if (gameStarted == 0) return;
+            for(int i = 0; i < 5; i++)
+            {
+                    if (lines[i].Top >= 970) lines[i].Top = -100;
+                    else lines[i].Top += speed;
+                //if (lines[i].Location.Y >= 900) lines[i].Location = new Point(lines[i].Location.X, -10);
+                //else lines[i].Location = new Point(lines[i].Location.X, lines[i].Location.Y + speed);
+            }
+           
+
+        }
+
+        void updateEnemy()
+        {
+            
+            for(int i = 0; i < 5; i++)
+            {
+                if (enemy[i].Top <= 1600) enemy[i].Top += speed;
+                else { enemy[i].Top = -400; enemy[i].Left = 10 + rand.Next(10, 500); }
+            }
+        }
+
+        void updateCoins()
+        {
+           for(int i = 0; i < 4; i++)
+            {
+                if (coins[i].Top >= 1020) { coins[i].Location = new Point(rand.Next(1, 10) * 50, -200); coins[i].Visible = true; }
+                else coins[i].Top += speed;
+            }
+        }
+
+        private void timerCloack_Tick(object sender, EventArgs e)
+        {
+            if (gameStarted == 0) return;
+            if (timeCounter < 35) timeCounter++;
+            if (timeCounter == 35 && speed <=21) { timeCounter = 0; speed++; }
+            //MessageBox.Show(timerCloack.Interval.ToString());
+            
+            moveLines();
+            updateEnemy();
+            updateCoins();
+
+            for(int i = 0; i < 5; i++)
+            {
+                if (Math.Abs(enemy[i].Top - player.Top) <= 185 && Math.Abs(enemy[i].Left - player.Left) <= 90) { timerCloack.Enabled = false; gameStarted = 0; endGame(); }
+            }
+
+            for(int i = 0; i < 4; i++)
+            {
+                if ( player.Top - coins[i].Top <= 45 && player.Top - coins[i].Top >= -200 && coins[i].Left - player.Left <= 90 && coins[i].Left - player.Left >= -60  && coins[i].Visible == true)
+                {
+                    currentScore++;
+                    coins[i].Visible = false;
+                    label2.Text = "Current Score :" + currentScore.ToString();
+                }
+            }
+            
+        }
+
+        void endGame()
+        {
+            btn_back.Enabled = true;
+            btn_back.Visible = true;
+            label2.Visible = false;
+            label3.Text = "You scored: " + currentScore.ToString();
+            label3.Visible = true;
+
+        }
+
+        private void Game_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            if (gameStarted == 0 || timerCloack.Enabled == false ) return;
+            if (e.KeyCode == Keys.Left && player.Left >= 10) player.Left -= speed;
+
+            if (e.KeyCode == Keys.Right && player.Left <= 520) player.Left += speed;
+            
+
+        }
+
+        private void Game_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           /* if (gameStarted == 0 || timerCloack.Enabled == false) return;
+            if (e.KeyChar == Left && player.Left >= 10) player.Left -= speed;
+
+            if (e.KeyChar == Right && player.Left <= 520) player.Left += speed;
+            */
+        }
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+            GameName.Visible = true;
+            if (currentScore > bestScore) bestScore = currentScore;
+            BestScoreLabel.Text = "Best Score: " + currentScore.ToString();
+            BestScoreLabel.Visible = true;
+            
+            Game.ActiveForm.BackColor = Color.Teal;
+            btnStartGame.Enabled = true;
+            btnStartGame.Visible = true;
+            leftBorder.Visible = false;
+            rightBorder.Visible = false;
+            label2.Visible = false;
+
+            panelGame.Controls.Remove(player);
+            for (int i = 0; i < 5; i++)
+                panelGame.Controls.Remove(lines[i]);
+            for (int i = 0; i < 5; i++)
+                panelGame.Controls.Remove(enemy[i]);
+            for (int i = 0; i < 4; i++)
+                panelGame.Controls.Remove(coins[i]);
+
+            label3.Visible = false;
+            btn_back.Visible = false;
+            btn_back.Enabled = false;
+        }
+    }
+}
